608a3fac3e5ce3b625641c0ff8693fcf7fabc38f
Dika yonanda putra <yonandaputra05@gmail.com>
2025-05-28 22:44:20 +0700

    Init Supabase

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:warkop_bunny/pages/home_page.dart';
 import 'package:warkop_bunny/themes/light_mode.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
-void main() {
+Future<void> main() async {
+  await Supabase.initialize(
+    url: 'https://wxwprcxzityepfhmlpsg.supabase.co',
+    anonKey:
+        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Ind4d3ByY3h6aXR5ZXBmaG1scHNnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDg0NDUzMDUsImV4cCI6MjA2NDAyMTMwNX0.LRady5Mgs-FZjOGD9fELjkMnnVbZislscD2reWi3A5c',
+  );
   runApp(const MyApp());
 }
 
