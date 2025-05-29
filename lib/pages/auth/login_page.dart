54b37296663061a246f018f60e1de435a90b65ea
Dika yonanda putra <yonandaputra05@gmail.com>
2025-05-29 08:58:28 +0700

    Update login logic

--- lib/pages/auth/login_page.dart
+++ lib/pages/auth/login_page.dart
@@ -1,35 +1,73 @@
 import 'package:flutter/material.dart';
+import 'package:warkop_bunny/auth/auth_service.dart';
 import 'package:warkop_bunny/components/my_button.dart';
 import 'package:warkop_bunny/components/my_text_field.dart';
+import 'package:warkop_bunny/pages/auth/register_page.dart';
+import 'package:warkop_bunny/pages/main_page.dart';
 
 class LoginPage extends StatefulWidget {
-  final void Function()? togglePages;
-
-  const LoginPage({super.key, this.togglePages});
+  const LoginPage({super.key});
 
   @override
   State<LoginPage> createState() => _LoginPageState();
 }
 
 class _LoginPageState extends State<LoginPage> {
+  // get auth service
+  final AuthService authService = AuthService();
+
   // text controller
   final emailController = TextEditingController();
   final passwordController = TextEditingController();
 
+  bool _isLoading = false;
+
   // login button pressed
-  void login() {
+  Future<void> login() async {
     final String email = emailController.text;
     final String password = passwordController.text;
 
-    // ensure that the email & password fields are not empty
-    if (email.isNotEmpty && password.isNotEmpty) {
-      // login logic here
-    } else {
-      // display error if some fields are empty
-      ScaffoldMessenger.of(context).showSnackBar(
-        const SnackBar(content: Text("Please enter both email and password")),
-      );
+    // attempt login..
+    try {
+      // ensure that the email & password fields are not empty
+      if (email.isNotEmpty && password.isNotEmpty) {
+        setState(() {
+          _isLoading = true;
+        });
+        // login logic
+        await authService.signInWithEmailPassword(email, password);
+
+          // navigate to MainPage and remove all previous pages
+        if (mounted) {
+          Navigator.pushAndRemoveUntil(
+            context,
+            MaterialPageRoute(builder: (context) => const MainPage()),
+            (route) => false,
+          );
+        }
+      } else {
+        // display error if some fields are empty
+        ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(content: Text("Please enter both email and password")),
+        );
+      }
+    } catch (e) {
+      ScaffoldMessenger.of(
+        context,
+      ).showSnackBar(SnackBar(content: Text("Error: $e")));
     }
+
+    setState(() {
+      _isLoading = false;
+    });
+  }
+
+  @override
+  void dispose() {
+    // dispose of the text controllers
+    emailController.dispose();
+    passwordController.dispose();
+    super.dispose();
   }
 
   // BUILD UI
@@ -45,6 +83,8 @@ class _LoginPageState extends State<LoginPage> {
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
+                const Text('Login'),
+
                 // logo
                 Icon(
                   Icons.lock_open_rounded,
@@ -84,7 +124,10 @@ class _LoginPageState extends State<LoginPage> {
                 const SizedBox(height: 25),
 
                 // login button
-                MyButton(onTap: login, text: "Login"),
+                if (_isLoading)
+                  const CircularProgressIndicator()
+                else
+                  MyButton(onTap: login, text: "Login"),
 
                 const SizedBox(height: 50),
 
@@ -93,13 +136,18 @@ class _LoginPageState extends State<LoginPage> {
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: [
                     Text(
-                      "don't have any account?",
+                      "don't have any account? ",
                       style: TextStyle(
                         color: Theme.of(context).colorScheme.primary,
                       ),
                     ),
                     GestureDetector(
-                      onTap: widget.togglePages,
+                      onTap: () => Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => const RegisterPage(),
+                        ),
+                      ),
                       child: Text(
                         "Register here",
                         style: TextStyle(
