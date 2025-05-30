6b9ce740b4dc6662604b67ab38a0affe31f7422a
Dika yonanda putra <yonandaputra05@gmail.com>
2025-05-31 06:44:37 +0700

    Add product controller

---
+++ lib/controller/product_controller.dart
@@ -0,0 +1,31 @@
+import 'package:supabase_flutter/supabase_flutter.dart';
+import 'package:warkop_bunny/models/product.dart';
+
+class ProductController {
+  // Database -> products
+  final product = Supabase.instance.client.from('products');
+
+  // Create
+  Future createProduct(Product newProduct) async {
+    await product.insert(newProduct.toMap());
+  }
+
+  // Read
+  final stream = Supabase.instance.client
+      .from('products')
+      .stream(primaryKey: ['id'])
+      .map(
+        (data) =>
+            data.map((productMap) => Product.fromMap(productMap)).toList(),
+      );
+
+  // Update
+  Future updateProduct(Product oldProduct, Product newProduct) async {
+    // await product.update(updatedProduct.toMap()).eq('id', oldProduct.id!);
+  }
+
+  // Delete
+  Future deleteProduct(Product product) async {
+    await this.product.delete().eq('id', product.id!);
+  }
+}
